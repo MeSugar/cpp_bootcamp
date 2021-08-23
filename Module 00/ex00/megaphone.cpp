6b7fb24dc3c0f53dcfadaa764d51e320e3afb484
MeSugar <vlasov3454@yandex.ru>
2021-08-23 21:25:37 +0300

    module 00 ex00 done

---
+++ Module 00/ex00/megaphone.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main(int ac, char **av)
+{
+	int	i;
+	int	j;
+
+	if (ac > 1)
+	{
+		i = 0;
+		while (av[++i])
+		{
+			j = -1;
+			while (av[i][++j])
+				std::cout << (char)std::toupper(av[i][j]);
+		}
+		std::cout << std::endl;
+	}
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	return (0);
+}
